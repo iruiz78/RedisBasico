fc56952bae6314d2189d519b58c0a39e7f786734
iruiz.incluit <ignacio.ruiz@incluit.com>
2023-04-07 11:45:32 -0300

    Se agrego ejemplos simples de set y get de objetos de Redis

---
+++ ApiRedis/Cache/Redis.cs
@@ -0,0 +1,32 @@
+﻿using ApiRedis.Modelos;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ApiRedis.Cache
+{
+    public class Redis
+    {
+        private static string _conection = "localhost";
+
+        public Redis(string conection) 
+        { 
+            _conection = conection;
+        }
+        public Redis(){}
+
+        public void SetData(string key, Cliente data) 
+        {
+            var redisConection = StackExchange.Redis.ConnectionMultiplexer.Connect(_conection);
+            var redis = redisConection.GetDatabase();
+            redis.StringSet(key, JsonSerializer.Serialize(data));        
+        }
+
+        public Cliente GetData(string key)
+        {
+            var redisConection = StackExchange.Redis.ConnectionMultiplexer.Connect(_conection);
+            var redis = redisConection.GetDatabase();
+            var cli = JsonSerializer.Deserialize<Cliente>(redis.StringGet(key));
+            return cli;
+        }
+    }
+}
---
+++ ApiRedis/Controllers/ClienteController.cs
@@ -0,0 +1,49 @@
+﻿using ApiRedis.Cache;
+using ApiRedis.Modelos;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiRedis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        // GET: api/<ClienteController>
+        [HttpGet]
+        public IEnumerable<Cliente> Get()
+        {
+            return new List<Cliente>();
+        }
+
+        // GET api/<ClienteController>/5
+        [HttpGet("{id}")]
+        public Cliente Get(int id)
+        {
+            var redisDb = new Redis();
+            var cli = redisDb.GetData($"Cliente-{id}");
+            return cli;
+        }
+
+        // POST api/<ClienteController>
+        [HttpPost]
+        public void Post([FromBody] Cliente value)
+        {
+            var redisDb = new Redis();
+            redisDb.SetData($"Cliente-{value.Id}", value);
+        }
+
+        // PUT api/<ClienteController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Cliente value)
+        {
+        }
+
+        // DELETE api/<ClienteController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
